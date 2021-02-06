e4ba5e8dc6e965a106afa62c7d395d96bc43c60d
UnaLuz <luz.cucarella97@gmail.com>
2021-02-05 21:57:45 -0300

    Added the base layout to add a new list item

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,18 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: hsl(80, 100, 80);
+  margin-top: 6vh;
+  display: flex;
+  justify-content: center;
 }
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="list">
-    A list
+    <label for="newItem">Write another list item:</label>
+    <input type="text" id="newItem" name="newItem" minlength="2" size="20" />
+    <button>Add</button>
   </div>
 </template>
 
@@ -11,12 +13,21 @@ export default {
 </script>
 
 <style scoped lang="scss">
-#app {
+#list {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+  padding: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+label,
+input {
+  margin: 0.5rem;
 }
 </style>
